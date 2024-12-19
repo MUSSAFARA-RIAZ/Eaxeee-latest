e3dedb9b3f21f3df51d4aaa6b30e5cb040388fa9
MUSSAFARA-RIAZ <mussafarariaz@gmail.com>
2024-12-19 17:50:43 +0500

    Modal ui fixes

--- src/views/Impex/ImpexComponents/Import_exportBox.js
+++ src/views/Impex/ImpexComponents/Import_exportBox.js
@@ -11,23 +11,23 @@ import DropDown from "./DropDown.js";
 import CustomButton from "../../../components/CustomButton/CustomButton.js";
 import AdminTranslation from "../../../Utils/AdminTranslation/AdminTranslation.js";
 import { connect } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect,useState } from "react";
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
 import ExportTemplateModal from "../Modals/ExportTemplateModal.js";
-import { getElementNames, getAllRepositories } from "../../../apis/impex_management.js";
+import { getElementNames,getAllRepositories } from "../../../apis/impex_management.js";
 const ImportExportBox = ({ props }) => {
   const [open, setOpen] = useState(false);
 
   const [listOfElements, setListOfElements] = useState([]);
   const [listOfDropdownElements, setListOfDropdownElements] = useState([]);
   const [selectedArchitecture, setSelectedArchitecture] = useState('');
-
-  //   const items = [
-  //     { id: 1, name: "Item 1" },
-  //     { id: 2, name: "Item 2" },
-  //     { id: 3, name: "Item 3" },
-  //     { id: 4, name: "Item 4" },
-  // ];
+  
+//   const items = [
+//     { id: 1, name: "Item 1" },
+//     { id: 2, name: "Item 2" },
+//     { id: 3, name: "Item 3" },
+//     { id: 4, name: "Item 4" },
+// ];
 
   const [modalConfig, setModalConfig] = useState({
     open: false,
@@ -46,7 +46,7 @@ const ImportExportBox = ({ props }) => {
 
     //   const res = exportTemplate
     // }
-    console.log("opened modal is: ", config)
+    console.log("opened modal is: ",config)
     // console.log("oelemnt list is:: ",listOfElements)
 
     setModalConfig({ open: true, ...config });
@@ -55,19 +55,19 @@ const ImportExportBox = ({ props }) => {
   const [isDisabled, setIsDisabled] = useState(false);
 
   const handleCloseModal = () => {
-
+   
     setIsDisabled(false);
 
     setModalConfig({ ...modalConfig, open: false });
-    console.log("disabled sae in handle close ", isDisabled);
+    console.log("disabled sae in handle close ",isDisabled);
   };
   // console.log("In import export box", props.theme);
+  
+ 
 
-
-
-  const handleDisabledChange = (value) => {
-    setIsDisabled(value); // Update the parent's state
-  };
+    const handleDisabledChange = (value) => {
+        setIsDisabled(value); // Update the parent's state
+    };
 
 
   useEffect(() => {
@@ -86,7 +86,7 @@ const ImportExportBox = ({ props }) => {
             id: index + 1,
             name: item
           }));
-
+          
           setListOfElements(transformedData);
         } else if (res.code === 401) {
           console.error("Unauthorized access:", res.data);
@@ -116,7 +116,7 @@ const ImportExportBox = ({ props }) => {
             id: item.id,
             architecture_name: item.architecture_name
           }));
-
+          
           setListOfDropdownElements(dropdownelements);
         } else if (res.code === 401) {
           console.error("Unauthorized access:", res.data);
@@ -208,7 +208,7 @@ const ImportExportBox = ({ props }) => {
               startIcon={<CloudUploadIcon />}
               onClick={() => {
                 if (!selectedArchitecture) {
-                  alert("Cannot proceed, selectedArchitecture is null");
+                alert("Cannot proceed, selectedArchitecture is null");
                   return;
                 }
                 handleOpenModal({
@@ -290,18 +290,18 @@ const ImportExportBox = ({ props }) => {
               loaderColor="success"
               loaderThickness={5}
               onClick={() => {
-                if (!selectedArchitecture) {
-                  alert("Cannot proceed, selectedArchitecture is null");
-                  return;
-                }
-                handleOpenModal({
-                  title: "Import Data",
-                  buttons: [
-                    { label: "Import", variant: "contained", onClick: () => console.log("Upload clicked") },
-                    { label: "Cancel", variant: "outlined", onClick: handleCloseModal },
-                  ],
-                });
-              }}
+  if (!selectedArchitecture) {
+    alert("Cannot proceed, selectedArchitecture is null");
+    return;
+  }
+  handleOpenModal({
+    title: "Import Data",
+    buttons: [
+      { label: "Import", variant: "contained", onClick: () => console.log("Upload clicked") },
+      { label: "Cancel", variant: "outlined", onClick: handleCloseModal },
+    ],
+  });
+}}
 
               startIcon={<CloudDownloadIcon />}
             />
